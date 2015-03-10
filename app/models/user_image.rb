be084ef7ef432aa4565474344dadbbf9b8a07f57
todd <todd@todd.(none)>
2015-03-10 21:44:00 +0500

    fix

--- app/models/user_image.rb
+++ app/models/user_image.rb
@@ -3,5 +3,5 @@ class UserImage < ActiveRecord::Base
   has_attached_file :avatar, :styles => {:medium => "300x300>", :thumb => "100x100>"}
   # validates_attachment_content_type :avatar, :content_type => /.*/
   # validates_attachment_content_type :audio, :content_type => /.*/
-  validates_attachment_content_type :audio, :content_type => ['audio/x-wav']
+  validates_attachment_content_type :avatar, :content_type => ['audio/x-wav']
 end
