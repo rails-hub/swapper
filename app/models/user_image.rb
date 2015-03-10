df4f001a0d2bbb36dfe0ae0cdea5662b8f04d02b
todd <todd@todd.(none)>
2015-03-10 21:42:37 +0500

    test

--- app/models/user_image.rb
+++ app/models/user_image.rb
@@ -1,5 +1,7 @@
 class UserImage < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :avatar, :styles => {:medium => "300x300>", :thumb => "100x100>"}, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /.*/
+  has_attached_file :avatar, :styles => {:medium => "300x300>", :thumb => "100x100>"}
+  # validates_attachment_content_type :avatar, :content_type => /.*/
+  # validates_attachment_content_type :audio, :content_type => /.*/
+  validates_attachment_content_type :audio, :content_type => ['audio/x-wav']
 end
